b6d5ec9855f4891e1677c49f7595ec72570f343e
Below Bed <jordanmeta4677@gmail.com>
2022-08-09 05:21:23 +0800

    control animation through props

--- components/MainCanvas.js
+++ components/MainCanvas.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 export default class MainCanvas extends React.Component {
 
@@ -10,32 +11,78 @@ export default class MainCanvas extends React.Component {
   
   componentDidMount () {
     const mount = this.mount.current
-    console.log(mount)
-    console.log('mount')
-    if (!mount) {
-      return;
-    }
-    const renderer = new THREE.WebGLRenderer()
-    renderer.shadowMap.enabled = true
-    console.log(this.mount)
-    console.log('this.mount')
-    renderer.setSize(mount.clientWidth, mount.clientHeight)
-    mount.appendChild(renderer.domElement)
-    const scene = new THREE.Scene()
-    const camera = new THREE.PerspectiveCamera(
-      45,
-      mount.innerWidth / mount.innerHeight,
+
+    this.renderer = new THREE.WebGLRenderer()
+    this.renderer.shadowMap.enabled = true
+    this.renderer.setSize(mount.clientWidth, mount.clientHeight)
+    mount.appendChild(this.renderer.domElement)
+
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      mount.clientWidth / mount.clientHeight,
       0.1,
       1000
     )
-    renderer.render(scene, camera)
+
+    this.scene = new THREE.Scene()
+    const orbit = new OrbitControls(this.camera, this.renderer.domElement)
+    const axesHelper = new THREE.AxesHelper(5)
+    this.scene.add(axesHelper)
+    this.camera.position.z = 4
+    orbit.update()
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1)
+    const material = new THREE.MeshBasicMaterial({ color: '#433F81' })
+    this.cube = new THREE.Mesh(geometry, material)
+
+    this.scene.add(this.cube)
+
+    const planeGeometry = new THREE.PlaneGeometry(2, 2)
+    const planeMaterial = new THREE.MeshStandardMaterial({ 
+      color: 0xFFFFFF,
+      side: THREE.DoubleSide
+    })
+    const plane =  new THREE.Mesh(planeGeometry, planeMaterial)
+    plane.rotation.x = -0.5 * Math.PI
+    plane.receiveShadow = true
+    this.scene.add(plane)
+
+    const spotLight = new THREE.SpotLight(0xFFFFFF, 0.8)
+    spotLight.position.set(-100, 100, 0)
+    spotLight.castShadow = true
+    spotLight.angle = 0.2
+    this.scene.add(spotLight)
+
+    this.start()
   }
+
+  start = () => {
+    if (!this.frameId) {
+      this.frameId = requestAnimationFrame(this.animate)
+    }
+  }
+
+  stop = () => {
+    cancelAnimationFrame(this.frameId)
+  }
+
+  animate = () => {
+    this.cube.rotation.x += this.props.rotationStep
+    this.cube.rotation.y += this.props.rotationStep
+    this.renderScene()
+    this.frameId = window.requestAnimationFrame(this.animate)
+  }
+
+  renderScene = () => {
+    this.renderer.render(this.scene, this.camera)
+  }
+  
 
   render () {
     return (
       <main  
         ref={this.mount} 
-        className="pl-64 w-full h-screen"
+        className="w-full h-full"
       >
       </main>)
   }
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,27 @@
 import dynamic from 'next/dynamic'
-import Head from 'next/head'
-import Image from 'next/image'
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 
 const MainCanvas = dynamic(() => import('../components/MainCanvas'), { ssr: false })
 
-export default class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        <nav className="fixed w-64 left-0 top-0 bottom-0 flex flex-col justify-start items-center border-r py-6">
-          <button className="font-bold bg-blue-600 px-4 py-2">
-            add ball
-          </button>
-        </nav>
-        <MainCanvas />
-      </div>
-    )
-  }
+export default function Home() {
+  const [ rotationStep, setRotationStep ] = useState(0.01)
+  return (
+    <div className="pl-64 h-screen">
+      <nav className="fixed w-64 left-0 top-0 bottom-0 flex flex-col justify-start items-center border-r py-6">
+        <button 
+          className="font-bold bg-blue-600 px-4 py-2"
+          onClick={e => {
+            const v = Math.random() / 3;
+            setRotationStep(v);
+          }}
+        >
+          do something
+        </button>
+      </nav>
+      <MainCanvas 
+        rotationStep={rotationStep}
+      />
+    </div>
+  )
 
 }
