d96adf0133f67ec5fab71cff53b03d38248eea96
Below Bed <jordanmeta4677@gmail.com>
2022-08-09 22:34:25 +0800

    enable control from react props

--- components/MainCanvas.js
+++ components/MainCanvas.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import * as THREE from 'three'
+import { STLLoader } from 'three/examples/jsm/loaders/STLLoader'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { DragControls } from 'three/examples/jsm/controls/DragControls'
 
 export default class MainCanvas extends React.Component {
+
+  trackedIds = [];
+  dragControls = null;
+  orbitControls = null;
 
   constructor (props) {
     super(props)
@@ -14,30 +20,32 @@ export default class MainCanvas extends React.Component {
 
     this.renderer = new THREE.WebGLRenderer()
     this.renderer.shadowMap.enabled = true
-    this.renderer.setSize(mount.clientWidth, mount.clientHeight)
+    this.renderer.setSize(mount.offsetWidth, mount.offsetHeight)
     mount.appendChild(this.renderer.domElement)
 
     this.camera = new THREE.PerspectiveCamera(
-      75,
-      mount.clientWidth / mount.clientHeight,
-      0.1,
-      1000
+        75,
+        mount.offsetWidth / mount.offsetHeight,
+        0.1,
+        1000
     )
+    this.camera.position.z = 100
+    this.camera.position.x = 50
+    this.camera.position.y = 100
 
     this.scene = new THREE.Scene()
-    const orbit = new OrbitControls(this.camera, this.renderer.domElement)
-    const axesHelper = new THREE.AxesHelper(5)
+    this.orbitControls = new OrbitControls(this.camera, this.renderer.domElement)
+    const axesHelper = new THREE.AxesHelper(1000)
     this.scene.add(axesHelper)
-    this.camera.position.z = 4
-    orbit.update()
+    this.orbitControls.update()
 
     const geometry = new THREE.BoxGeometry(1, 1, 1)
     const material = new THREE.MeshBasicMaterial({ color: '#433F81' })
     this.cube = new THREE.Mesh(geometry, material)
 
-    this.scene.add(this.cube)
+    // this.scene.add(this.cube)
 
-    const planeGeometry = new THREE.PlaneGeometry(2, 2)
+    const planeGeometry = new THREE.PlaneGeometry(100, 100)
     const planeMaterial = new THREE.MeshStandardMaterial({ 
       color: 0xFFFFFF,
       side: THREE.DoubleSide
@@ -45,15 +53,35 @@ export default class MainCanvas extends React.Component {
     const plane =  new THREE.Mesh(planeGeometry, planeMaterial)
     plane.rotation.x = -0.5 * Math.PI
     plane.receiveShadow = true
-    this.scene.add(plane)
+    // this.scene.add(plane)
 
-    const spotLight = new THREE.SpotLight(0xFFFFFF, 0.8)
-    spotLight.position.set(-100, 100, 0)
-    spotLight.castShadow = true
-    spotLight.angle = 0.2
-    this.scene.add(spotLight)
+    const light = new THREE.SpotLight()
+    light.position.set(50, 100, 100)
+    this.scene.add(light)
+
+    const sLightHelper = new THREE.SpotLightHelper(light)
+    this.scene.add(sLightHelper)
+
+    this.rayCaster = new THREE.Raycaster()
+    this.mousePosition = new THREE.Vector2()
+    mount.addEventListener('pointermove', this.onMouseMove)
 
     this.start()
+  }
+
+  onMouseMove = (e) => {
+    const mount = this.mount.current
+    let renderWidth = mount.offsetWidth;
+    let renderHeight = mount.offsetHeight;
+    // Get normalized coordinates
+    // Must subtract window.inner against mount.size
+    this.mousePosition.x = ((e.layerX - 256) / renderWidth) * 2 - 1
+    this.mousePosition.y = - (e.layerY / renderHeight) * 2 + 1
+  }
+
+  componentWillUnmount() {
+    const mount = this.mount.current
+    mount.removeEventListener('pointermove', this.onMouseMove)
   }
 
   start = () => {
@@ -67,8 +95,28 @@ export default class MainCanvas extends React.Component {
   }
 
   animate = () => {
-    this.cube.rotation.x += this.props.rotationStep
-    this.cube.rotation.y += this.props.rotationStep
+    // if (this.mesh) {
+    //   this.mesh.rotation.y += this.props.rotationStep
+    // }
+    this.rayCaster.setFromCamera(this.mousePosition, this.camera)
+    const intersects = this.rayCaster.intersectObjects(this.scene.children)
+    let isHit = false
+    for (let i = 0; i < intersects.length; i++) {
+      const id = intersects[i].object.id
+      if (this.trackedIds.includes(id)) {
+        isHit = true
+        intersects[i].object.material.color.set(0xFF0000)
+      }
+    }
+    if (!isHit) {
+      this.trackedIds.forEach(id => {
+        const obj = this.scene.getObjectById(id)
+        if (obj) {
+          obj.material.color.set(0xb2ffc8)
+        }
+      })
+    }
+
     this.renderScene()
     this.frameId = window.requestAnimationFrame(this.animate)
   }
@@ -76,7 +124,51 @@ export default class MainCanvas extends React.Component {
   renderScene = () => {
     this.renderer.render(this.scene, this.camera)
   }
-  
+
+  componentDidUpdate = () => {
+    if (this.props.stlFile) {
+      console.log('got stl file')
+      const loader = new STLLoader()
+      loader.load(
+          this.props.stlFile,
+          (geometry) => {
+            const material = new THREE.MeshBasicMaterial({
+              color: 0xb2ffc8
+            })
+            const mesh = new THREE.Mesh(geometry, material)
+            this.trackedIds.push(mesh.id)
+            this.scene.add(mesh)
+            this.addDragControl()
+          },
+          (xhr) => {
+              console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+          },
+          (error) => {
+              console.log(error)
+          }
+      )
+    }
+  }
+
+  addDragControl = () => {
+    // remove previous control
+    if (this.dragControls)  {
+      this.dragControls.dispose()
+      delete this.dragControls
+    }
+    const objects = this.trackedIds.map(id => {
+      return this.scene.getObjectById(id)
+    })
+    this.dragControls = new DragControls(objects, this.camera, this.renderer.domElement)
+    this.dragControls.addEventListener('dragstart', (event) => {
+        this.orbitControls.enabled = false
+        event.object.material.opacity = 0.33
+    })
+    this.dragControls.addEventListener('dragend', (event) => {
+        this.orbitControls.enabled = true
+        event.object.material.opacity = 1
+    })
+  }
 
   render () {
     return (
--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,14 @@ const MainCanvas = dynamic(() => import('../components/MainCanvas'), { ssr: fals
 
 export default function Home() {
   const [ rotationStep, setRotationStep ] = useState(0.01)
+
+  const [ stlFile, setStlFile ] = useState()
+  const [ stlProp, setStlProp ] = useState()
+
   return (
     <div className="pl-64 h-screen">
-      <nav className="fixed w-64 left-0 top-0 bottom-0 flex flex-col justify-start items-center border-r py-6">
+
+      <nav className="fixed w-64 left-0 top-0 bottom-0 flex flex-col justify-start items-center border-r py-6 px-4 flex flex-col gap-4">
         <button 
           className="font-bold bg-blue-600 px-4 py-2"
           onClick={e => {
@@ -17,9 +22,29 @@ export default function Home() {
         >
           do something
         </button>
+
+        <div className="flex flex-col gap-2">
+          <label className="text-sm text-gray-500">Load an STL File</label>
+          <input
+            className="border px-4 py-2 bg-gray-50 rounded overflow-hidden w-full"
+            type="text"
+            onChange={e => {
+              setStlFile(e.target.value)
+            }}
+          />
+          <button 
+            onClick={() => {
+              if (stlFile) {
+                setStlProp(stlFile)
+              }
+            }}
+            className="text-sm font-bold px-2 py-1 rounded bg-blue-500 text-white"
+          >load file</button>
+        </div>
       </nav>
       <MainCanvas 
         rotationStep={rotationStep}
+        stlFile={stlProp}
       />
     </div>
   )
